758aa1a6b4636e66919ec13d04a6d6c4555f3ef6
christgb <srsteve1913@gmail.com>
2023-10-01 05:49:16 -0400

    dto-validations

--- src/clients/clients.dto/clientDto.ts
+++ src/clients/clients.dto/clientDto.ts
@@ -1,27 +1,35 @@
-import { isString } from 'class-validator'
+import { IsIn, IsInt, IsNotEmpty, IsNumber, IsOptional, IsString, MinLength } from 'class-validator'
 import { AppointmentsStatus } from '../client.entity'
 
-
-export class Client {
-    id:string
-    name:string
-    id_cedula:string
-    age:number
-    tel:string
-    appointment: AppointmentsStatus
-}
-
 export class DtoClient {
+    @IsString()
+    @IsNotEmpty()
     name:string
+    @IsNotEmpty()
+    @MinLength(11)
     id_cedula:string
+    @IsNotEmpty()
+    @IsNumber()
     age:number
+    @IsNotEmpty()
+    @IsString()
     tel:string
 }
 
 export class DtoUpdatedClient {
+    @IsString()
+    @IsOptional()
     name:string
+    @IsString()
+    @IsOptional()
     id_cedula:string
+    @IsNumber()
+    @IsOptional()
     age:number
+    @IsString()
+    @IsOptional()
     tel:string
+    @IsIn([AppointmentsStatus.NONE, AppointmentsStatus.SOMETHING])
+    @IsOptional()
     appointment: AppointmentsStatus
 }
