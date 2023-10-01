df90cf49c24ac49526d86254cbcc012bb4deda73
christgb <srsteve1913@gmail.com>
2023-10-01 06:50:41 -0400

    storageInMemory

---
+++ src/db/storageInMemory.ts
@@ -0,0 +1,24 @@
+import { Client } from "src/clients/client.entity"
+import { AppointmentsStatus } from "src/clients/client.entity"
+import { Procedures } from "src/patient-appointments/appointment.entity"
+import { v4 } from "uuid"
+
+export const clientInMemory:Client = {
+    id:v4(),
+    name:"Chris",
+    id_cedula:"402-3181906-7",
+    age:21,
+    tel:"829-533-2795",
+    appointment:AppointmentsStatus.NONE,
+}
+
+export const procedimientos: Procedures = { 
+    name_procedures: ["H", "H"]
+ };
+  
+  
+  
+  
+  
+  
+  
