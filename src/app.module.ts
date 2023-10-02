319c1533b8bafb2bf70ffeb98b3990a49acb411e
christgb <srsteve1913@gmail.com>
2023-10-02 04:02:28 -0400

    connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from './clients/clients.module';
-import { DentalProcedureModule } from './dental-procedure/dental-procedure.module';
 import { PatientAppointmentsModule } from './patient-appointments/patient-appointments.module';
 import { UserAppointmentController } from './user-appointment/user-appointment.controller';
 import { UserAppointmentService } from './user-appointment/user-appointment.service';
+import { DentalRecordModule } from './dental-record/dental-record.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ClientsModule, DentalProcedureModule, PatientAppointmentsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host:'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'testDatabase',
+    database: 'odotdb',
+    entities: [__dirname + '/**/*.entity{.ts,.js}']
+  }), ClientsModule, PatientAppointmentsModule, DentalRecordModule],
   controllers: [UserAppointmentController],
   providers: [UserAppointmentService],
 })
