1beb18165cb4864baa8bbcc3529a80cb02e1b193
christgb <srsteve1913@gmail.com>
2023-10-02 17:06:12 -0400

    typeORM feat

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
+import { Client } from './client.entity';
+import { ClientsController } from './clients.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Client])],
+  providers: [ClientsService], 
   controllers: [ClientsController],
-  providers: [ClientsService]
 })
-export class ClientsModule {
-  
-}
+export class ClientsModule {}
+
