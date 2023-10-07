76e1d0db3d12e61dc618c09c19202c8f7f810f77
christgb <srsteve1913@gmail.com>
2023-10-07 03:24:37 -0400

    Error handler

--- src/appointment/appointment.service.ts
+++ src/appointment/appointment.service.ts
@@ -4,19 +4,20 @@ import { CreateAppointmentDto, UpdateAppointmentDto } from './dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { Service } from 'src/service/service.entity';
 import Sequelize from 'sequelize';
+import { NotFoundException, BadRequestException } from '@nestjs/common';
 
 @Injectable()
 export class AppointmentsService {
-    
-    constructor(@InjectModel(Appointment) private appointmentModel: typeof Appointment,
-    @InjectModel(Service) private serviceModel: typeof Service){}
 
-    async getAppointments(): Promise<Appointment[]>{
+    constructor(@InjectModel(Appointment) private appointmentModel: typeof Appointment,
+        @InjectModel(Service) private serviceModel: typeof Service) { }
+
+    async getAppointments(): Promise<Appointment[]> {
         const appointmentWithService = await Appointment.findAll({
             include: [
                 {
                     model: Service,
-                    as: 'service', 
+                    as: 'service',
                     attributes: ['id', 'name', 'cost']
                 }
             ]
@@ -24,59 +25,84 @@ export class AppointmentsService {
         return appointmentWithService;
     }
 
-    async getAppointmentById(id:number): Promise<Appointment>{
-        return this.appointmentModel.findOne({
-            where: {
-                id:id
-            }
-        });
-    }
- 
-    async createAppointment(dto: CreateAppointmentDto) {
-        const services = await Service.findAll({
-            where: {
-                id: {
-                    [Sequelize.Op.in]: dto.serviceIds
+    async getAppointmentById(id: number): Promise<Appointment> {
+        if (id !== null && undefined) {
+            return this.appointmentModel.findOne({
+                where: {
+                    id: id
                 }
-            }
-        });
-    
+            });
+        } else {
+            throw new Error("El ID no es valido")
+        }
+    }
+
+    async createAppointment(dto: CreateAppointmentDto) {
+        let services: Service[] = null;
+
+        if (dto.serviceIds !== null && undefined) {
+            services = await Service.findAll({
+                where: {
+                    id: {
+                        [Sequelize.Op.in]: dto.serviceIds
+                    }
+                }
+            });
+        }
+
         if (services.length === dto.serviceIds.length) {
             const appointmentData = {
                 appointmentDate: dto.appointmentDate,
                 notes: dto.notes,
                 patientId: dto.patientId
             };
-    
+
             const appointment = await this.appointmentModel.create(appointmentData);
-            
+
             await appointment.$set('service', services);
-    
+
             return appointment;
         } else {
             throw new Error('Algunos IDs de servicio proporcionados no son válidos.');
         }
     }
 
-    async updateAppointment(dto: UpdateAppointmentDto, id:number){
+    async updateAppointment(dto: UpdateAppointmentDto, id: number) {
+        if (id <= 0) {
+            throw new BadRequestException('El ID de la cita no es válido')
+        }
+
         const appointment = await this.appointmentModel.findOne({
-            where:{
-                id:id
+            where: {
+                id: id
             }
         });
 
-        await appointment.update(dto);
-    }
-
-    async deleteAppointment(id:number){
-      const appointment = await this.appointmentModel.findOne({
-        where:{
-            id:id
+        if(!appointment){
+            throw new NotFoundException('Cita no encontrada')
         }
-      });
 
-      return appointment.destroy()
-    
+        await appointment.update(dto);
+
     }
+
+    async deleteAppointment(id: number) {
+        if (id <= 0) {
+            throw new BadRequestException('El ID de la cita no es válido');
+        }
     
+        const appointment = await this.appointmentModel.findOne({
+            where: {
+                id: id,
+            },
+        });
+    
+        if (!appointment) {
+            throw new NotFoundException('Cita no encontrada');
+        }
+    
+        await appointment.destroy();
+        return 'Cita eliminada correctamente';
+    }
+
 }
